9ffd4715f5a378ce84a20339da590c0a785de8d3
Kenneth Chung <kennethchung5@gmail.com>
2019-01-25 23:55:31 -0500

    added placeholder content: question text and friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,6 +10,21 @@ var friendsArray = [
         "name": "FriendTwo",
         "photo": "https://astronlogia.com/wp-content/uploads/2016/12/2.png?x59474",
         "scores": ["1","2","1","2","1","2","1","2","1","2"]
+    },
+    {
+        "name": "FriendThree",
+        "photo": "http://www.printable-alphabets.com/wp-content/uploads/2012/10/number-solid-3.jpg",
+        "scores": ["4","1","2","3","5","3","4","4","2","3"]
+    },
+    {
+        "name": "FriendFour",
+        "photo": "https://vignette.wikia.nocookie.net/phobia/images/d/d0/4.jpg/revision/latest?cb=20161127143001",
+        "scores": ["4","2","3","4","1","1","5","2","4","3"]
+    },
+    {
+        "name": "FriendFive",
+        "photo": "https://www.westenddiy.co.uk/ekmps/shops/ab2451/images/classic-designs-3-black-number-5-digit-pack-5-35629-p.jpg",
+        "scores": ["5","3","5","3","4","1","3","2","3","5"]
     }
 ];
 
